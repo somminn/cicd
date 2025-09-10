8f716bf00da0be813157e456b5463735679b46ca
somminn <cowmin104@gmail.com>
2025-09-10 16:07:39 +0900

    commit

--- src/main/java/com/example/cicd/TestController.java
+++ src/main/java/com/example/cicd/TestController.java
@@ -8,6 +8,6 @@ import org.springframework.web.bind.annotation.RestController;
 public class TestController {
     @GetMapping("/test")
     public ResponseEntity test() {
-        return ResponseEntity.status(200).body("test 성공 완료 32");
+        return ResponseEntity.status(200).body("test 성공 완료 33");
     }
 }
