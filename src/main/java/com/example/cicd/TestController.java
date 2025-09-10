3ac77e1c0b52fae864ab7c385ab2f59a9b4da69a
somminn <cowmin104@gmail.com>
2025-09-10 10:11:38 +0900

    [Feat] test

---
+++ src/main/java/com/example/cicd/TestController.java
@@ -0,0 +1,13 @@
+package com.example.cicd;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class TestController {
+    @GetMapping("/test")
+    public ResponseEntity test() {
+        return ResponseEntity.status(200).body("test 성공 완료");
+    }
+}
